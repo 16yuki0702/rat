b092a36f5448d5b4fac6c672c7669a5458fca2c6
16yuki0702 <hs19870702@gmail.com>
2017-06-07 07:50:28 +0000

    [modify]fixed indent.

--- main.c
+++ main.c
@@ -11,14 +11,14 @@ typedef struct {
 	char *host;
 	char *protocol;
 	uint16_t backlog;
-    int socket_reuse;
+	int socket_reuse;
 } rat_conf;
 
 char *
 conf_handler_string(const char *param)
 {
 	char *ret;
-        int len;
+	int len;
 
 	ret = strdup(param);
 	len = strlen(ret);
@@ -56,7 +56,7 @@ read_config(char *path)
 	while (fgets(buf, 1024, f)) {
 		token = strtok_r(buf, " ", &cptr);
 		if (!strcmp(token, "#")) {
-            // skip comment section.
+			// skip comment section.
 		} else if (!strcmp(token, "host")) {
 			conf->host = conf_handler_string(strtok_r(NULL, "", &cptr));
 		} else if (!strcmp(token, "port")) {
@@ -77,40 +77,40 @@ int
 open_socket(rat_conf *conf)
 {
 	int server_socket, client_socket;
-    int server_len, client_len;
-    int read_size;
+	int server_len, client_len;
+	int read_size;
 	struct sockaddr_in server_addr, client_addr;
-    const int yes = 1;
+	const int yes = 1;
 
 	server_socket = socket(AF_INET, SOCK_STREAM, 0);
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(conf->port);
 	server_addr.sin_addr.s_addr = INADDR_ANY;
-    if (conf->socket_reuse == 1) {
-        setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &conf->socket_reuse, sizeof(conf->socket_reuse));
-        printf("reuse option.\n");
-    }
+	if (conf->socket_reuse == 1) {
+		setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &conf->socket_reuse, sizeof(conf->socket_reuse));
+		printf("reuse option.\n");
+	}
 	bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
 	listen(server_socket, conf->backlog);
 
-    while (1) {
-        char read_buffer[1024];
-        memset(read_buffer, 0, sizeof(read_buffer));
+	while (1) {
+		char read_buffer[1024];
+		memset(read_buffer, 0, sizeof(read_buffer));
 
-        client_len = sizeof(client_addr);
-        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_len);
-        if (client_socket == -1) {
-            printf("failed open socket.\n");
-            return -1;
-        }
+		client_len = sizeof(client_addr);
+		client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_len);
+		if (client_socket == -1) {
+			printf("failed open socket.\n");
+			return -1;
+		}
 
-        read_size = read(client_socket, read_buffer, sizeof(read_buffer));
-        if (read_size == -1) {
-            printf("failed read socket.\n");
-            return -1;
-        }
-        printf("%s\n", read_buffer);
-    }
+		read_size = read(client_socket, read_buffer, sizeof(read_buffer));
+		if (read_size == -1) {
+			printf("failed read socket.\n");
+			return -1;
+		}
+		printf("%s\n", read_buffer);
+	}
 
 	close(server_socket);
 	close(client_socket);
