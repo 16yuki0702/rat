db1aec5edca0fa7d58681f46952b2effe6b0c2ae
16yuki0702 <hs19870702@gmail.com>
2017-06-28 08:00:31 +0000

    [modify]add null string method.

--- rat_string.h
+++ rat_string.h
@@ -6,6 +6,7 @@ typedef struct {
 	size_t len;
 } rat_str;
 
-#define rat_string(str)	{ (char *)str, sizeof(str) - 1 }
+#define rat_string(str)		{ (char *)str, sizeof(str) - 1 }
+#define rat_null_string(str)	{ NULL, 0 }
 
 #endif
