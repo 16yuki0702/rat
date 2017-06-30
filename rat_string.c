17ffeceb31a14785c455b3ea502998f3f8bfd138
16yuki0702 <hs19870702@gmail.com>
2017-06-30 13:37:49 +0000

    [modify]trim method.

--- rat_string.c
+++ rat_string.c
@@ -1,21 +1,21 @@
 #include "rat_string.h"
 
 void
-rat_str_trim(rat_str *str)
+rat_str_trim(char *str)
 {
         int i;
+
         if (str == NULL) {
                 return;
         }
 
-        i = strlen(str->data);
-        while (--i >= 0 && str->data[i] == ' ');
+        i = strlen(str);
 
-        str->data[i + 1] = '\0';
+        while (--i >= 0 && str[i] == ' ');
+
+        str[i + 1] = '\0';
 
         i = 0;
-        while (str->data[i] != '\0' && str->data[i] == ' ') i++;
-
-        strcpy(str->data, &str->data[i]);
-	str->len = strlen(str->data);
+        while (str[i] != '\0' && str[i] == ' ') i++;
+        strcpy(str, &str[i]);
 }
--- rat_string.h
+++ rat_string.h
@@ -1,7 +1,9 @@
 #ifndef RAT_STRING_H
 #define RAT_STRING_H
 
+#include <stdio.h>
 #include <stddef.h>
+#include <string.h>
 
 typedef struct {
 	char *data;
@@ -11,6 +13,6 @@ typedef struct {
 #define rat_string(str)		{ (char *)str, sizeof(str) - 1 }
 #define rat_null_string(str)	{ NULL, 0 }
 
-extern void rat_str_trim(rat_str *str);
+extern void rat_str_trim(char *str);
 
 #endif
