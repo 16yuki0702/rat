5445051189e34fbae1a5edd1cd07ab6b1613577f
16yuki0702 <hs19870702@gmail.com>
2017-07-16 15:10:48 +0000

    [modify]add log method.

--- main.c
+++ main.c
@@ -1,13 +1,36 @@
+#include <stdio.h>
 #include <signal.h>
 #include "server.h"
 #include "config.h"
 
+FILE *rat_log_file;
+
 #define LOG(level, str)				\
 	do {					\
 		if (rat_log_level >= level) { 	\
+			_log_prefix(__func__);	\
 			_log x;			\
 		}				\
 	} while (0)
+
+void _log_prefix(const char *func) {
+	char prefix[21];
+	strncpy(prefix, func, 20);
+	prefix[20] = '\0';
+	if (rat_log_file == NULL) {
+		rat_log_file = stderr;
+	}
+	fprintf(rat_log_file, "%-20s ", prefix);
+}
+
+void _log(const char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+	vfprintf(rat_log_file, fmt, ap);
+	va_end(ap);
+	fputc('\n', rat_log_file);
+	fflush(rat_log_file);
+}
 
 int rat_log_level = 0;
 
