26f6c02a21fd564b1d3003ea61c9693e6de8eab2
16yuki0702 <hs19870702@gmail.com>
2017-09-28 14:40:57 +0000

    [modify]add mqtt topic parser.

--- rat_mqtt.c
+++ rat_mqtt.c
@@ -1,5 +1,82 @@
 #include "rat_mqtt.h"
 
+char test1[] = "foo";
+char test2[] = "foo/bar";
+char test3[] = "foo/bar/baz";
+
+typedef struct _map_entry {
+	_map_entry *root;
+	_map_entry *left;
+	_map_entry *right;
+	char *data;
+} map_entry;
+
+typedef struct {
+	map_entry **roots;
+	int count;
+} mapper;
+
+mapper map;
+
+typedef enum {
+	SAME = 0,
+	NOT_SAME,
+	LEFT,
+	RIGHT
+} CMP_RES;
+
+CMP_RES
+_strcmpn(char *s1, char *s2, int len)
+{
+	int i;
+	for (i = 0; i < len; i++) {
+		if (!s1[i] && !s2[i]) {
+			return SAME;
+		} else if (!s1[i] && s2[i]) {
+			return RIGHT;
+		} else if (s1[i] && !s2[i]) {
+			return LEFT;
+		} else if (s1[i] != s2[i]) {
+			return NOT_SAME;
+		}
+	}
+
+	return SAME;
+}
+
+void
+_parse_topic(char *e_data, char *t)
+{
+	char *start;
+	int c = 0;
+	CMP_RES r;
+
+	while (t[c]) {
+		start = t;
+		if (t[c] == '/') {
+			r = _strcmpn(data, t, strlen(t));
+		}
+		c++;
+	}
+}
+
+void
+_realloc_map(int resize)
+{
+	map.roots = (map_entry**)realloc(sizeof(map_entry*) * resize);
+}
+
+void
+_test_driver()
+{
+	_realloc_map(1);
+	_parse_topic(test1);
+	_realloc_map(2);
+	_parse_topic(test2);
+	_realloc_map(3);
+	_parse_topic(test3);
+}
+
 void
 _dec2bin(int dec)
 {
