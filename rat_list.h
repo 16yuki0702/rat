6a37c2b5412faced956daacf501b979115d05a12
16yuki0702 <hs19870702@gmail.com>
2017-10-16 06:05:41 +0000

    [modify]list.

--- rat_list.h
+++ rat_list.h
@@ -9,11 +9,24 @@ typedef struct _list {
 	_list *next;
 } r_list;
 
-#define LIST_INIT (x)		\
-	do {			\
-		x->data = NULL;	\
-		x->prev = NULL;	\
-		x->next = NULL;	\
+#define LIST_INIT (x)						\
+	do {							\
+		x->data = NULL;					\
+		x->prev = NULL;					\
+		x->next = NULL;					\
+	} while (0)
+
+#define LIST_ENTRY (x, d)					\
+	do {							\
+		r_list *tmplist = x;				\
+		while (tmplist) {				\
+			if (tmplist->data) {			\
+				tmplist = tmlist->next;		\
+			} else {				\
+				tmplist->data = d;		\
+				break;				\
+			}					\
+		}						\
 	} while (0)
 
 #endif
